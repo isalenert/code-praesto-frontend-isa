ced19cfcbb9917e1c00922552917042d59f1255d
until99 <gabriel.kasten@outlook.com>
2024-04-13 17:24:49 -0300

    added tailwind

--- src/pages/App.js
+++ src/pages/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
-      {/* <Acaccordion />*/}
+      {/* <Acaccordion /> */}
       <Section section="participate" />
 
       {/* <Card-List /> */}
