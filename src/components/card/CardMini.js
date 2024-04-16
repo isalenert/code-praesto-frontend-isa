7e700d10780c91e7a4bcbb3df74934e78fc3deb2
until99 <gabriel.kasten@outlook.com>
2024-04-16 00:04:24 -0300

    static cards section

--- src/components/card/CardMini.js
+++ src/components/card/CardMini.js
@@ -1,26 +1,28 @@
-import TeslaLogo from "../../assets/bussines-logo.png";
 
-function CardMin() {
+
+function CardMin({href, coverImage, title, bussinessName, price, duration, qntParticipants, description}) {
   return (
-    <div className="border-solid border-2 border-gray-200 p-6 rounded-2xl mt-8">
+    <a href={href}>
+      <div className="border-solid border-2 border-gray-200 p-6 rounded-2xl mt-8">
       <div className="flex flex-row justify-between">
         <div className="flex flex-col space-y-1">
-          <p className="font-bold text-xl">HackTech 2024</p>
-          <p className="font-medium text-base">Tech Innovations Ltda.</p>
+          <p className="font-bold text-xl">{title}</p>
+          <p className="font-medium text-base">{bussinessName}</p>
         </div>
         <div className="w-6/12 flex justify-end">
-          <img src={TeslaLogo} />
+          <img src={coverImage} alt="cover image"/>
         </div>
       </div>
-      <div className="flex flex-col space-y-2 mt-6 text-gray-900">
+      <div className="flex flex-col space-y-2 mt-6 text-gray-600">
         <div className="flex flex-row justify-between">
-          <p className="text-sm font-medium">10 de abril - 12 de abril</p>
-          <p className="text-sm font-medium"><strong>200</strong> participantes</p>
+          <p className="text-sm font-medium">{duration}</p>
+          <p className="text-sm font-medium"><strong>{qntParticipants}</strong> participantes</p>
         </div>
-        <p className="text-sm font-medium line-clamp-3 text-justify">Desafio de programação de 48 horas para criar soluções inovadoras usando tecnologias de ponta.</p>
-        <p className="text-sm font-bold">U$ 10.000,00</p>
+        <p className="text-sm font-medium line-clamp-3 text-justify">{description}</p>
+        <p className="text-sm font-bold pt-4 line-clamp-1">{price}</p>
       </div>
     </div>
+    </a>
   )
 }
 
