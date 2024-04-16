d970c98632086e6dc8f7ac8a20be1c92f94591f9
until99 <gabriel.kasten@outlook.com>
2024-04-15 23:32:36 -0300

    mini card component and card section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
+// components
 import Navbar from "../components/layout/Navbar";
 import Hero from "../components/layout/Hero";
 import Section from "../components/layout/Section";
 import Footer from "../components/layout/Footer";
+import CardSection from '../components/layout/CardSection';
+
+// assets
+import FlameIcon from "../assets/icons/Fire.png"
 
 function HomePage() {
   return (
     <div>
       <Navbar />
       <Hero />
+      
       {/* <Acaccordion /> */}
-      <Section section="participate" />
 
-      {/* <Card-List /> */}
+      <Section section="participate" />
+      
+      <CardSection className="mt-40" title="Populares" href={"/test"} iconPath={FlameIcon}>
+        <p>Card</p>
+      </CardSection>
 
       <Section section="oraganize" />
       <Footer />
